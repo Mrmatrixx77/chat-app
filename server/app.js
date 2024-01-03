0defe5a14b4c598aa340566e34716ed14196b9a9
Mrmatrixx77 <kunalsk2001@gmail.com>
2024-01-03 12:06:00 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from "socket.io";
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+const __dirname = dirname(fileURLToPath(import.meta.url)); 
+app.use(express.static('server'));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+
+io.on('connection' , (Socket) => {
+    console.log("connection");
+    Socket.on("send-name",(myname)=>{
+      io.emit("send-name",myname);
+    });
+    Socket.on("message",(message)=>{
+      io.emit("message",message);
+    });
+})
+
+
+httpServer.listen(8000,() =>{
+    console.log(`app is running on port 8000`)
+});
